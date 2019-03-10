2921f0a7c5c55d36a58d9df73fd776a1af840810
Lucas Wohlhart <lucas@wohlhart.at>
2019-03-10 11:57:32 +0100

    feat(nav-test): add test script for navigation stack

---
+++ src/ShellScripts/test_navigation.sh
@@ -0,0 +1,15 @@
+
+#!/bin/sh
+
+WORLD_PATH=$( cd "$(dirname "$0")/../World" ; pwd -P )
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$WORLD_PATH/lw_house.world" &
+sleep 5
+
+xterm -e "roslaunch turtlebot_navigation amcl_demo.launch map_file:=$WORLD_PATH/lw_house_map.yaml" &
+sleep 5
+
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+# sleep 5
+
+# xterm -e "roslaunch turtlebot_teleop keyboard_teleop.launch"
+
